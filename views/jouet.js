f9c98b33f297ae01525cd061b4b9059ce1ee7206
Noamgicquiaud <noam.gic@gmail.com>
2017-09-26 14:25:16 +0200

    Affichage d'un jouet (ajout attribut id)

--- views/jouet.js
+++ views/jouet.js
@@ -1,6 +1,7 @@
 class Jouet {
 
-    constructor(unLibelle,uneTrancheAge,uneCategorie){
+    constructor(unId, unLibelle,uneTrancheAge,uneCategorie){
+        this.id = unId;
         this._libelle = unLibelle;
         this._trancheAge = uneTrancheAge;
         this._categorie = uneCategorie;
@@ -17,6 +18,10 @@ class Jouet {
 
     get categorie(){
         return this._categorie.libelle;
+    };
+
+    convenir(unAge){
+        return (unAge >= this._trancheAge.ageMin) && (unAge <= this._trancheAge.ageMax)
     }
 }
 module.exports= Jouet;
