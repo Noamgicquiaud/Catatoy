9ca71494547f37474600419b8a72c9378168b205
Noamgicquiaud <noam.gic@gmail.com>
2017-09-26 10:48:46 +0200

    Affectation d'une categorie à un jouet

--- jouet.js
+++ jouet.js
@@ -1,8 +1,11 @@
 class Jouet {
 
-    constructor(unLibelle,uneTrancheAge){
+    constructor(unLibelle,uneTrancheAge,uneCategorie){
         this._libelle = unLibelle;
         this._trancheAge = uneTrancheAge;
+        this._categorie = uneCategorie;
+
+        this._categorie.ajouterJouet(this)
     }
 
     get libelle(){
@@ -12,5 +15,8 @@ class Jouet {
         return this._trancheAge.toString();
     }
 
+    get categorie(){
+        return this._categorie.libelle;
+    }
 }
 module.exports= Jouet;
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const Jouet = require('./jouet.js');
 const TrancheAge = require('./trancheAge.js');
+const Categorie = require('./categorie');
 
 console.log("Bienvenue dans le catalogue de jouet");
 
 let tranche25 =  new TrancheAge("tranche25",2,5);
-console.log(tranche25.toString());
+//console.log(tranche25.toString());
 
-let unJouet = new Jouet("Draisienne junior bleue",tranche25);
-console.log(unJouet.trancheAge);
+let categorie1 = new Categorie("Plein Air");
+//console.log(categorie1.libelle);
+
+let unJouet = new Jouet("Draisienne junior bleue",tranche25,categorie1);
+//console.log(unJouet.trancheAge);
+
+categorie1;afficherJouets(console.log);
